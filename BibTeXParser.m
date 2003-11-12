8283b174ecac6a86c378d05c93c27d88b9274847
mmcc <mmcc@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2003-11-12 01:40:53 +0000

    Added a check to see if the scanner actually scanned any characters - if it didn't, then we end up referencing invalid memory.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@171 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibTeXParser.m
+++ BibTeXParser.m
@@ -156,9 +156,9 @@
                         validscan = [NSScanner scannerWithString:s];  //Scan string s after we get it from bt
                         
                         [validscan setCharactersToBeSkipped:nil]; //If the first character is a newline or whitespace, NSScanner will skip it by default, which gives a bad length value
-                        [validscan scanCharactersFromSet:asciiLetters intoString:&validscanstring];
+                        BOOL scannedCharacters = [validscan scanCharactersFromSet:asciiLetters intoString:&validscanstring];
                         
-                        if([validscanstring length] != [s length]) //Compare it to the original string
+                        if(scannedCharacters && ([validscanstring length] != [s length])) //Compare it to the original string
                         {
                             NSLog(@"This string was in the file: [%@]",s);
                             NSLog(@"This is the part we can read: [%@]",validscanstring);
