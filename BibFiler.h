ae1e51bc3a883ec6a15ef39e3225b0e01d8d8f05
mmcc <mmcc@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2004-05-02 08:58:22 +0000

    Added singleton object to handle organizing papers folder (AutoFile)
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@525 0fceea05-a30d-0410-8a8b-80ef821fb0a1

---
+++ BibFiler.h
@@ -0,0 +1,20 @@
+//
+//  BibFiler.h
+//  Bibdesk
+//
+//  Created by Michael McCracken on Fri Apr 30 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BibPrefController.h"
+@class BibDocument;
+
+@interface BibFiler : NSObject {
+
+}
+
++ (BibFiler *)sharedFiler;
+- (void)filePapers:(NSArray *)papers fromDocument:(BibDocument *)doc;
+
+@end
