c194adac29fa7ce1c6006d410d2113b7d82e9017
mmcc <mmcc@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2004-06-30 18:53:28 +0000

    new AutoFile preview feature, shows window with info and configuration, allows cancel, and deleting duplicate files.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@575 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibFiler.h
+++ BibFiler.h
@@ -12,10 +12,36 @@
 @class BibDocument;
 
 @interface BibFiler : NSObject {
-
+	NSMutableArray *_fileInfoDicts;
+	
+	IBOutlet NSWindow *window;
+	IBOutlet NSTableView *tv;
+	IBOutlet NSButton *actionButton;
+	IBOutlet NSButton *cancelButton;
+	IBOutlet NSTextField *infoTextField;
+	IBOutlet NSButton *cleanupCheckBox;
+	IBOutlet NSButton *deleteCheckBox;
+	
+	NSArray *_currentPapers;
+	BibDocument *_currentDocument;
+	NSString *_errorString;
+	int _moveCount;
+	int _movableCount;
+	int _deletedCount;
+	int _cleanupChangeCount;
 }
 
 + (BibFiler *)sharedFiler;
-- (void)filePapers:(NSArray *)papers fromDocument:(BibDocument *)doc;
+
+- (void)showPreviewForPapers:(NSArray *)papers fromDocument:(BibDocument *)doc;
+- (void)doMoveAction:(id)sender;
+- (IBAction)cancelFromPreview:(id)sender;
+- (void)doCleanup;
+- (void)file:(BOOL)doFile papers:(NSArray *)papers fromDocument:(BibDocument *)doc;
+
+- (BOOL)fileManager:(NSFileManager *)manager shouldProceedAfterError:(NSDictionary *)errorInfo;
+
+- (IBAction)handleCleanupLinksAction:(id)sender;
+
 
 @end
--- BibFiler.m
+++ BibFiler.m
@@ -19,8 +19,76 @@ static BibFiler *_sharedFiler = nil;
 	return _sharedFiler;
 }
 
-- (void)filePapers:(NSArray *)papers fromDocument:(BibDocument *)doc{
+- (id)init{
+	if(self = [super init]){
+		_fileInfoDicts = [[NSMutableArray arrayWithCapacity:10] retain];
+		
+	}
+	return self;
+}
+
+- (void)dealloc{
+	[_fileInfoDicts release];
+	[super dealloc];
+}
+
+#pragma mark Preview / Postmortem Info
+
+- (void)showPreviewForPapers:(NSArray *)papers fromDocument:(BibDocument *)doc{
+	
+	BOOL success = [NSBundle loadNibNamed:@"AutoFile" owner:self];
+	if(!success){
+		// @@ throw up error boxs
+	}
+	_currentPapers = papers;
+	_currentDocument = doc;
+	[self file:NO papers:papers fromDocument:doc];
+
+	if(_movableCount == 0){
+		NSRunAlertPanel(NSLocalizedString(@"No files to consolidate",@""),
+						NSLocalizedString(@"All the linked files point to the Papers Folder.",@""),
+						NSLocalizedString(@"OK",@""),nil,nil);
+		return;
+	}
+	
+	[tv reloadData];
+	NSString *papersFolderPath = [[OFPreferenceWrapper sharedPreferenceWrapper] stringForKey:BDSKPapersFolderPathKey];
+	papersFolderPath = [papersFolderPath stringByAbbreviatingWithTildeInPath];
+	
+	[infoTextField setStringValue:[NSString stringWithFormat:
+		NSLocalizedString(@"These files that will be moved to %@.\nScan this list for errors, because this operation cannot be undone.",@"description string"), papersFolderPath]];
+	[window makeKeyAndOrderFront:self];	
+}
+
+- (void)doMoveAction:(id)sender{
+	[self file:YES papers:_currentPapers fromDocument:_currentDocument];
+	
+	NSRunAlertPanel(NSLocalizedString(@"Files Consolidated", @""),
+					NSLocalizedString(@"%d of %d eligible files were moved.\n%d links were fixed, %d duplicate files were deleted.",@""),
+					NSLocalizedString(@"OK",@"OK"),
+					nil, nil, _moveCount, _movableCount, _cleanupChangeCount, _deletedCount);
+	
+	[self doCleanup];
+}
+
+- (IBAction)cancelFromPreview:(id)sender{
+	[self doCleanup];
+}
+
+- (void)doCleanup{
+	_currentPapers = nil;
+	_currentDocument = nil;
+	[window close];
+}
+
+- (void)file:(BOOL)doFile papers:(NSArray *)papers fromDocument:(BibDocument *)doc{
 	NSFileManager *fm = [NSFileManager defaultManager];
+	
+	_moveCount = 0;
+	_movableCount = 0;
+	_deletedCount = 0;
+	_cleanupChangeCount = 0;
+	[_fileInfoDicts removeAllObjects];
 	
 	foreach(paper , papers){
 		NSString *path = [paper localURLPathRelativeTo:[[(NSDocument *)doc fileName] stringByDeletingLastPathComponent]];
@@ -28,27 +96,92 @@ static BibFiler *_sharedFiler = nil;
 		NSString *newPath = [[OFPreferenceWrapper sharedPreferenceWrapper] stringForKey:BDSKPapersFolderPathKey];
 		newPath = [newPath stringByAppendingPathComponent:fileName];
 		
+		NSMutableDictionary *info = [NSMutableDictionary dictionaryWithObjectsAndKeys:[path stringByAbbreviatingWithTildeInPath], @"oloc", 
+			newPath, @"nloc", nil];
+		NSString *status = nil;
+	
 		if(path){
+			NSString *fileURLString = [[NSURL fileURLWithPath:newPath] absoluteString];
+
 			if(![path isEqualToString:newPath]){
-				NSLog(@"filing path: %@\nnew path: %@", path, newPath);
-				
-				if(![fm fileExistsAtPath:newPath]){
-					if([fm movePath:path toPath:newPath handler:nil]){
-						
-						NSString *fileURLString = [[NSURL fileURLWithPath:newPath] absoluteString];
-						
-						[paper setField:@"Local-Url" toValue:fileURLString];
+				if([fm fileExistsAtPath:newPath]){
+					BOOL pathExists = [fm fileExistsAtPath:path];
+					if(pathExists){
+						status = NSLocalizedString(@"A copy of the linked file is in your Papers Folder.",@"");
 					}else{
-						NSLog(@"there was an error moving the file %@", path);
+						status = NSLocalizedString(@"The linked file does not exist, but a file with the same name is in the Papers Folder.", @"");
+					}
+					if(doFile){
+						if(pathExists && [deleteCheckBox state] == NSOnState){
+							[fm removeFileAtPath:path handler:nil];
+							_deletedCount++;
+						}
+						if([cleanupCheckBox state] == NSOnState){
+							[paper setField:@"Local-Url" toValue:fileURLString];
+							_cleanupChangeCount++;
+						}
 					}
 				}else{
-					NSLog(@"a file that name is already there"); // Throw a bdskerrobj
+					if(doFile){
+						if([fm movePath:path toPath:newPath handler:self]){
+							[paper setField:@"Local-Url" toValue:fileURLString];
+							status = NSLocalizedString(@"success", @"success"); // won't appear
+							_moveCount++;
+						}else{
+							status = [_errorString autorelease];
+						}
+					}else{
+						status = NSLocalizedString(@"OK to move", @"ok to move");
+					}
 				}
-				
+				_movableCount++;
+				[info setObject:status forKey:@"status"];
+				[_fileInfoDicts addObject:info];
 			}
-			
 		}
+			
 	}
+	if(_moveCount > 0 || _deletedCount > 0 || _cleanupChangeCount > 0){
+		[doc updateChangeCount:NSChangeDone];
+	}
+
+}
+
+- (BOOL)fileManager:(NSFileManager *)manager shouldProceedAfterError:(NSDictionary *)errorInfo{
+	_errorString = [[errorInfo objectForKey:@"Error"] retain];
+	return NO;
+}
+
+- (IBAction)handleCleanupLinksAction:(id)sender{
+	if([sender state] == NSOnState){
+		[deleteCheckBox setEnabled:YES];
+	}else{
+		[deleteCheckBox setEnabled:NO];
+	}
+}
+
+#pragma mark table view stuff
+
+- (int)numberOfRowsInTableView:(NSTableView *)tableView{
+	return [_fileInfoDicts count]; 
+}
+
+- (id)tableView:(NSTableView *)tableView 
+objectValueForTableColumn:(NSTableColumn *)tableColumn 
+			row:(int)row{
+	NSString *tcid = [tableColumn identifier];
+	NSDictionary *dict = [_fileInfoDicts objectAtIndex:row];
+	
+	if([tcid isEqualToString:@"icon"]){
+		return @"-";
+	}else if([tcid isEqualToString:@"oloc"]){
+		return [dict objectForKey:@"oloc"];
+	}else if([tcid isEqualToString:@"nloc"]){
+		return [dict objectForKey:@"nloc"];
+	}else if([tcid isEqualToString:@"status"]){
+		return [dict objectForKey:@"status"];
+	}
+	else return @"??";
 }
 
 @end
