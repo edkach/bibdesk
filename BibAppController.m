1833135933b7b24f7396da4d2bbd9e3985a1bfde
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2004-10-21 04:35:11 +0000

    Fix a crasher that would occur when opening a MacOSRoman encoded document as UTF8 (nil string).
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@841 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibAppController.m
+++ BibAppController.m
@@ -291,6 +291,12 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 	NSString *content = [[[NSString alloc] initWithData:data encoding:encoding] autorelease];  // this returns a Unicode string
 	BibDocument *doc = nil;
 	
+	if(content == nil){ // ARM:  This happens if you try to open a MacRoman encoded file as UTF8, and then the loadBibTeXDataRepresentation: fails somewhere (I didn't bother tracing it any further).
+	    NSRunAlertPanel(NSLocalizedString(@"Error",@"Error"),
+			    NSLocalizedString(@"Incorrect file encoding chosen.  Please try to reopen the file with a different encoding.",@"Wrong encoding message."), nil, nil, nil);
+	    return;
+	}	    
+	
 	doc = [[NSDocumentController sharedDocumentController] openUntitledDocumentOfType:@"bibTeX database" display:YES];
 	[doc loadBibTeXDataRepresentation:[[BDSKConverter stringByTeXifyingString:content] dataUsingEncoding:NSUTF8StringEncoding]];
 	[doc updateChangeCount:NSChangeDone];
