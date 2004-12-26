73ae0ecb784984e97266a91d0c9fda8432d3e0c2
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2004-12-26 17:06:01 +0000

    Fix in case the crasher using NSWindowController as a tablevew datasource affects the person controller as well.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@1211 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibPersonController.m
+++ BibPersonController.m
@@ -38,6 +38,8 @@
 #if DEBUG
     NSLog(@"personcontroller dealloc");
 #endif
+    [pubsTableView setDataSource:nil];
+    [pubsTableView setDelegate:nil];
     [_person release];
     [publications release];
     [super dealloc];
