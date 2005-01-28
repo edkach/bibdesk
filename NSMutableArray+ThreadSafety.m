e0184f2cc6e5f415921a0115d5710c02fb8007b2
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-01-28 06:07:01 +0000

    Add thread safe additions for collections, based on Apple TN2059.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@1347 0fceea05-a30d-0410-8a8b-80ef821fb0a1

---
+++ NSMutableArray+ThreadSafety.m
@@ -0,0 +1,76 @@
+//
+//  NSMutableArray+ThreadSafety.m
+//  Bibdesk
+//
+//  Created by Adam Maxwell on 01/27/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSMutableArray+ThreadSafety.h"
+
+
+@implementation NSMutableArray (ThreadSafety)
+
+- (void)addObject:(id)anObject usingLock:(NSLock *)aLock{
+    
+    [aLock lock];
+    [[anObject retain] autorelease];
+    [self addObject:anObject];
+    [aLock unlock];    
+}
+
+- (void)insertObject:(id)anObject atIndex:(unsigned)index usingLock:(NSLock *)aLock{
+    
+    [aLock lock];
+    [[anObject retain] autorelease];
+    [self insertObject:anObject atIndex:index];
+    [aLock unlock];    
+}
+
+- (id)objectAtIndex:(unsigned)index usingLock:(NSLock *)aLock{
+    
+    id result;
+    
+    [aLock lock];
+    result = [self objectAtIndex:index];
+    [[result retain] autorelease];
+    [aLock unlock];
+    
+    return result;
+}
+    
+- (void)removeObjectAtIndex:(unsigned)index usingLock:(NSLock *)aLock{
+    
+    [aLock lock];
+    [self removeObjectAtIndex:index];
+    [aLock unlock];
+}
+
+- (void)removeObject:(id)anObject usingLock:(NSLock *)aLock{
+    
+    [aLock lock];
+    [self removeObject];
+    [aLock unlock];
+}
+
+- (BOOL)containsObject:(id)anObject usingLock:(NSLock *)aLock{
+    
+    BOOL yn;
+    
+    [aLock lock];
+#warning should retain anObject?
+    yn = [self containsObject:anObject];
+    [aLock unlock];
+    
+    return yn;
+}
+
+- (void)removeObjectIdenticalTo:(id)anObject usingLock:(NSLock *)aLock{
+    
+    [aLock lock];
+    [self removeObjectIdenticalTo:anObject];
+    [aLock unlock];
+}
+
+    
+@end
