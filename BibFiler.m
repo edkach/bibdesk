e3376c483dc845629db1f68eab4318aa9d012cb9
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-02-12 22:17:35 +0000

    Remove old old autofile methods and some small edits in new one.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@1445 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibFiler.m
+++ BibFiler.m
@@ -45,7 +45,7 @@ static BibFiler *_sharedFiler = nil;
 		// The directory isn't there or isn't a directory, so pop up an alert.
 		rv = NSRunAlertPanel(NSLocalizedString(@"Papers Folder doesn't exist",@""),
 							 NSLocalizedString(@"The Papers Folder you've chosen either doesn't exist or isn't a folder. Any files you have dragged in will be linked to in their original location. Press \"Go to Preferences\" to set the Papers Folder.",@""),
-							 NSLocalizedString(@"OK",@""),NSLocalizedString(@"Go to Preferences",@""),nil);
+							 NSLocalizedString(@"OK",@"OK"),NSLocalizedString(@"Go to Preferences",@""),nil);
 		if (rv == NSAlertAlternateReturn){
 				[[OAPreferenceController sharedPreferenceController] showPreferencesPanel:self];
 				[[OAPreferenceController sharedPreferenceController] setCurrentClientByClassName:@"BibPref_AutoFile"];
@@ -93,9 +93,9 @@ static BibFiler *_sharedFiler = nil;
 	if(moveAll || [paper canSetLocalUrl]){
 		if([fm fileExistsAtPath:newPath]){
 			if([fm fileExistsAtPath:path]){
-				status = NSLocalizedString(@"A file with the generated file name exists; the linked file exists.",@"");
+				status = NSLocalizedString(@"Generated file path exists and the linked file exists.",@"");
 			}else{
-				status = NSLocalizedString(@"A file with the generated file name exists; the linked file does not exist.", @"");
+				status = NSLocalizedString(@"Generated file path exists and the linked file does not exist.", @"");
 			}
 		}else{
 			if([fm movePath:path toPath:newPath handler:self]){
@@ -111,7 +111,7 @@ static BibFiler *_sharedFiler = nil;
 			}
 		}
 	}else{
-		status = NSLocalizedString(@"Incomplete bibliography information to generate the file name.",@"");
+		status = NSLocalizedString(@"Incomplete information to generate the file name.",@"");
 	}
 	_movableCount++;
 	
@@ -151,76 +151,13 @@ static BibFiler *_sharedFiler = nil;
 	}
 
 	[tv reloadData];
-	[infoTextField setStringValue:NSLocalizedString(@"The following files had problems moving to the generated file location.",@"description string")];
-	[tv setDoubleAction:@selector(openFile:)];
+	[infoTextField setStringValue:NSLocalizedString(@"There were problems moving the follolwing files to the generated file location, according to the format string.",@"description string")];
+	[tv setDoubleAction:@selector(showFile:)];
 	[tv setTarget:self];
 	[window makeKeyAndOrderFront:self];
 }
 
 - (IBAction)done:(id)sender{
-	[self doCleanup];
-}
-
-
-#pragma mark Preview / Postmortem Info
-
-- (void)showPreviewForPapers:(NSArray *)papers fromDocument:(BibDocument *)doc{
-	
-	NSString *papersFolderPath = [[OFPreferenceWrapper sharedPreferenceWrapper] stringForKey:BDSKPapersFolderPathKey];
-
-	NSFileManager *fm = [NSFileManager defaultManager];
-	BOOL isDir;
-	
-	if(!([fm fileExistsAtPath:papersFolderPath isDirectory:&isDir] && isDir)){
-		// The directory isn't there or isn't a directory, so pop up an alert.
-		int rv = NSRunAlertPanel(NSLocalizedString(@"Papers Folder doesn't exist",@""),
-								 NSLocalizedString(@"The Papers Folder you've chosen either doesn't exist or isn't a folder. Press \"Go to Preferences\" to set the Papers Folder.",@""),
-								 NSLocalizedString(@"OK",@""),NSLocalizedString(@"Go to Preferences",@""),nil);
-		if (rv == NSAlertAlternateReturn){
-			[[OAPreferenceController sharedPreferenceController] showPreferencesPanel:self];
-			[[OAPreferenceController sharedPreferenceController] setCurrentClientByClassName:@"BibPref_AutoFile"];
-		}
-		return;
-	}
-	
-	
-	BOOL success = [NSBundle loadNibNamed:@"AutoFile" owner:self];
-	if(!success){
-		NSRunCriticalAlertPanel(NSLocalizedString(@"Error loading AutoFile window module.",@""),
-								NSLocalizedString(@"There was an error loading the AutoFile window module. BibDesk will still run, and automatically filing papers that are dragged in should still work fine. Please report this error to the developers. Sorry!",@""),
-								NSLocalizedString(@"OK",@""),nil,nil);
-		return;
-	}
-	_currentPapers = papers;
-	_currentDocument = doc;
-	[self file:NO papers:papers fromDocument:doc];
-
-	if(_movableCount == 0){
-		NSRunAlertPanel(NSLocalizedString(@"No files to consolidate",@""),
-						NSLocalizedString(@"All the linked files point to the Papers Folder.",@""),
-						NSLocalizedString(@"OK",@""),nil,nil);
-		return;
-	}
-	
-	[tv reloadData];
-	
-	[infoTextField setStringValue:[NSString stringWithFormat:
-		NSLocalizedString(@"These files that will be moved to %@.\nScan this list for errors, because this operation cannot be undone.",@"description string"), [papersFolderPath stringByAbbreviatingWithTildeInPath]]];
-	[window makeKeyAndOrderFront:self];	
-}
-
-- (void)doMoveAction:(id)sender{
-	[self file:YES papers:_currentPapers fromDocument:_currentDocument];
-	
-	NSRunAlertPanel(NSLocalizedString(@"Files Consolidated", @""),
-					NSLocalizedString(@"%d of %d eligible files were moved.\n%d links were fixed, %d duplicate files were deleted.",@""),
-					NSLocalizedString(@"OK",@"OK"),
-					nil, nil, _moveCount, _movableCount, _cleanupChangeCount, _deletedCount);
-	
-	[self doCleanup];
-}
-
-- (IBAction)cancelFromPreview:(id)sender{
 	[self doCleanup];
 }
 
@@ -230,103 +167,9 @@ static BibFiler *_sharedFiler = nil;
 	[window close];
 }
 
-- (void)file:(BOOL)doFile papers:(NSArray *)papers fromDocument:(BibDocument *)doc{
-
-	NSFileManager *fm = [NSFileManager defaultManager];
-	NSString *papersFolderPath = [[OFPreferenceWrapper sharedPreferenceWrapper] stringForKey:BDSKPapersFolderPathKey];
-	
-	BOOL isDir;
-	// Because we don't call file:papers: from the preview window unless the directory exists,
-	// this will only be executed if we're not coming from there. We won't get a repeat error.
-	
-	BOOL fileExists = [fm fileExistsAtPath:papersFolderPath isDirectory:&isDir];
-		
-	if(!([fm fileExistsAtPath:papersFolderPath isDirectory:&isDir] && isDir)){
-		// The directory isn't there or isn't a directory, so pop up an alert.
-		int rv = NSRunAlertPanel(NSLocalizedString(@"Papers Folder doesn't exist",@""),
-								 NSLocalizedString(@"The Papers Folder you've chosen either doesn't exist or isn't a folder. Any files you have dragged in will be linked to in their original location. Press \"Go to Preferences\" to set the Papers Folder.",@""),
-								 NSLocalizedString(@"OK",@""),NSLocalizedString(@"Go to Preferences",@""),nil);
-		if (rv == NSAlertAlternateReturn){
-			[[OAPreferenceController sharedPreferenceController] showPreferencesPanel:self];
-			[[OAPreferenceController sharedPreferenceController] setCurrentClientByClassName:@"BibPref_AutoFile"];
-		}
-		return;
-		
-	}
-	
-	_moveCount = 0;
-	_movableCount = 0;
-	_deletedCount = 0;
-	_cleanupChangeCount = 0;
-	[_fileInfoDicts removeAllObjects];
-	
-	foreach(paper , papers){
-		NSString *path = [paper localURLPathRelativeTo:[[(NSDocument *)doc fileName] stringByDeletingLastPathComponent]];
-		NSString *fileName = [path lastPathComponent];
-		NSString *newPath = [papersFolderPath stringByAppendingPathComponent:fileName];
-		
-		NSMutableDictionary *info = [NSMutableDictionary dictionaryWithObjectsAndKeys:[path stringByAbbreviatingWithTildeInPath], @"oloc", 
-			newPath, @"nloc", nil];
-		NSString *status = nil;
-	
-		if(path){
-			NSString *fileURLString = [[NSURL fileURLWithPath:newPath] absoluteString];
-
-			if(![path isEqualToString:newPath]){
-				if([fm fileExistsAtPath:newPath]){
-					BOOL pathExists = [fm fileExistsAtPath:path];
-					if(pathExists){
-						status = NSLocalizedString(@"A copy of the linked file is in your Papers Folder.",@"");
-					}else{
-						status = NSLocalizedString(@"The linked file does not exist, but a file with the same name is in the Papers Folder.", @"");
-					}
-					if(doFile){
-						if(pathExists && [deleteCheckBox state] == NSOnState){
-							[fm removeFileAtPath:path handler:nil];
-							_deletedCount++;
-						}
-						if([cleanupCheckBox state] == NSOnState){
-							[paper setField:BDSKLocalUrlString toValue:fileURLString];
-							_cleanupChangeCount++;
-						}
-					}
-				}else{
-					if(doFile){
-						if([fm movePath:path toPath:newPath handler:self]){
-							[paper setField:BDSKLocalUrlString toValue:fileURLString];
-							status = NSLocalizedString(@"success", @"success"); // won't appear
-							_moveCount++;
-						}else{
-							status = [_errorString autorelease];
-						}
-					}else{
-						status = NSLocalizedString(@"OK to move", @"ok to move");
-					}
-				}
-				_movableCount++;
-				[info setObject:status forKey:@"status"];
-				[_fileInfoDicts addObject:info];
-			}
-		}
-			
-	}
-	if(_moveCount > 0 || _deletedCount > 0 || _cleanupChangeCount > 0){
-		//[doc updateChangeCount:NSChangeDone];
-	}
-
-}
-
 - (BOOL)fileManager:(NSFileManager *)manager shouldProceedAfterError:(NSDictionary *)errorInfo{
 	_errorString = [[errorInfo objectForKey:@"Error"] retain];
 	return NO;
-}
-
-- (IBAction)handleCleanupLinksAction:(id)sender{
-	if([sender state] == NSOnState){
-		[deleteCheckBox setEnabled:YES];
-	}else{
-		[deleteCheckBox setEnabled:NO];
-	}
 }
 
 #pragma mark table view stuff
@@ -381,6 +224,10 @@ objectValueForTableColumn:(NSTableColumn *)tableColumn
 	}else if([tcid isEqualToString:@"nloc"]){
 		path = [[dict objectForKey:@"nloc"] stringByExpandingTildeInPath];
 		[[NSWorkspace sharedWorkspace]  selectFile:path inFileViewerRootedAtPath:nil];
+	}else if([tcid isEqualToString:@"status"]){
+		NSRunAlertPanel(nil,
+						[dict objectForKey:@"status"],
+						NSLocalizedString(@"OK",@"OK"),nil,nil);
 	}
 }
 
