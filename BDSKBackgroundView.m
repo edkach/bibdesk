06e83c6528eb39ce91f0c387a27badba8fb58983
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-02-28 08:23:05 +0000

    Add shadow and make white semi-transparent.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@1600 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKBackgroundView.m
+++ BDSKBackgroundView.m
@@ -11,11 +11,21 @@
 @implementation BDSKBackgroundView
 - (void)drawRect:(NSRect)rect
 {
-	[[NSColor controlColor] set];
-	[NSBezierPath fillRect:rect];
-	[[NSColor lightGrayColor] set];
-	[NSBezierPath setDefaultLineWidth:2];
-	[NSBezierPath strokeRect:rect];
+	float blur = 4.0;
+	float offset = 2.0;
+	NSSize size = [self bounds].size;
+	NSRect viewRect = NSMakeRect(blur, blur + offset, size.width - 2 * blur, size.height - blur - offset);
+	NSShadow *viewShadow = [[[NSShadow alloc] init] autorelease];
+	[viewShadow setShadowOffset:NSMakeSize(0.0, - offset)];
+	[viewShadow setShadowBlurRadius:blur];
+	[viewShadow setShadowColor:[[NSColor blackColor] colorWithAlphaComponent:0.6]];
+	
+	[NSGraphicsContext saveGraphicsState];
+	[viewShadow set];
+ 	[[[NSColor controlBackgroundColor] colorWithAlphaComponent:0.9] set]; // this is the white control background
+	[NSBezierPath fillRect:viewRect];
+	[NSGraphicsContext restoreGraphicsState];
+	
 	[super drawRect:rect];
 }
 @end
