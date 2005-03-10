03583f608ade1e39599c6ebe16be4a2632642b1f
mmcc <mmcc@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-03-10 00:35:21 +0000

    New files for new display architecture for native keyed-archive file format. BDSKItemDisplayController.h: has a protocol defining itemdisplaycontrollers. There is no corresponding .m file. BDSKItemBibTeXDisplayController.m,h: A simple display controller that displays bibtex in a textview. BDSKItemSource.h : a protocol defining item sources. Pretty simple. BDSKItemTableDisplayController.m,h: A display controller to do what the current table view in bibdocument does now. I forgot these files were in here. They're probably fully unimplemented. BDSKLibrary.m,h: The new NSDocument subclass - this is data model only! No UI. Getting pretty close to done with this one, it supports undo for adding/removing publications and sends some appropriate notifications. Needs undo/notification support for adding new collections. BDSKLibraryController.m,h: The new NSWindowController that handles the UI for the BDSKLibrary. It handles the sourcelist, searchfield, and coordinating the itemdisplaycontrollers. BDSKRemoteSource.m,h: This class isn't really implemented yet. It's mostly a placeholder now, and is probably going to become a subclass of BibCollection. BibNote.h,m: Similarly a placeholder.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@1691 0fceea05-a30d-0410-8a8b-80ef821fb0a1

---
+++ BDSKRemoteSource.h
@@ -0,0 +1,63 @@
+//
+//  BDSKRemoteSource.h
+//  Bibdesk
+//
+//  Created by Michael McCracken on 2/11/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+/*!
+    @class BDSKRemoteSource
+    @abstract   A superclass for sources.
+    @discussion (description)
+*/
+#warning : shouldn't this become a subclass of bibcollection?
+@interface BDSKRemoteSource : NSObject{
+	NSString *name;
+	NSMutableDictionary *data;
+}
+
+/*!
+* @method name
+ * @abstract the getter corresponding to setName
+ * @result returns value for name
+ */
+- (NSString *)name;
+	/*!
+	* @method setName
+	 * @abstract sets name to the param
+	 * @discussion 
+	 * @param aName 
+	 */
+- (void)setName:(NSString *)aName;
+
+
+	/*!
+	* @method data
+	 * @abstract the getter corresponding to setData
+	 * @result returns value for data
+	 */
+- (NSMutableDictionary *)data;
+	/*!
+	* @method setData
+	 * @abstract sets data to the param
+	 * @discussion 
+	 * @param aData 
+	 */
+- (void)setData:(NSMutableDictionary *)aData;
+
+
+    /*!
+    @method     settingsView
+     @abstract   returns a view that contains controls for setting up the source
+     @discussion (description)
+     @result     (description)
+     */
+- (NSView *)settingsView;
+
+- (NSArray *)items;
+- (void)refresh;
+
+@end
---
+++ BDSKRemoteSource.m
@@ -0,0 +1,66 @@
+//
+//  BDSKRemoteSource.m
+//  Bibdesk
+//
+//  Created by Michael McCracken on 2/11/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "BDSKRemoteSource.h"
+
+
+@implementation BDSKRemoteSource
+
+// init
+- (id)init {
+    if (self = [super init]) {
+        [self setName:nil];
+        [self setData:nil];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [super encodeWithCoder:coder];
+    [coder encodeObject:[self name] forKey:@"name"];
+    [coder encodeObject:[self data] forKey:@"data"];
+}
+
+- (id)initWithCoder:(NSCoder *)coder {
+    if ([super initWithCoder:coder]) {
+        [self setName:[coder decodeObjectForKey:@"name"]];
+        [self setData:[coder decodeObjectForKey:@"data"]];
+    }
+    return self;
+}
+
+- (NSString *)name { return [[name retain] autorelease]; }
+
+- (void)setName:(NSString *)aName {
+    //NSLog(@"in -setName:, old value of name: %@, changed to: %@", name, aName);
+	
+    [name release];
+    name = [aName copy];
+}
+
+- (NSMutableDictionary *)data { return [[data retain] autorelease]; }
+
+- (void)setData:(NSMutableDictionary *)aData {
+    //NSLog(@"in -setData:, old value of data: %@, changed to: %@", data, aData);
+	
+    [data release];
+    data = [aData copy];
+}
+
+- (NSView *)settingsView{
+    [NSException raise:NSInternalInconsistencyException format:@"Must implement a complete subclass."];
+    return nil;
+}
+
+- (void)dealloc {
+    [name release];
+    [data release];
+    [super dealloc];
+}
+
+@end
