ecc55e300deee4c20cd52e51322e50c946217420
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-04-05 20:45:21 +0000

    My previous change to searching was unnecessary; we already search by substring, to there's no point in removing TeX command, but do we need to remove braces.  This also fixes a bug, in that braces were not removed in all cases.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@1917 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibDocument.m
+++ BibDocument.m
@@ -1529,7 +1529,7 @@ stringByAppendingPathComponent:@"BibDesk"]; */
                 if(!value){
                     r.location = NSNotFound;
                 } else {
-                    value = [NSString lossyASCIIStringWithString:[value stringByRemovingTeX]];
+                    value = [NSString lossyASCIIStringWithString:[value stringByRemovingCurlyBraces]];
                     r = [value rangeOfString:componentSubstring
                                      options:NSCaseInsensitiveSearch];
                 }
@@ -1548,7 +1548,7 @@ stringByAppendingPathComponent:@"BibDesk"]; */
             pubEnum = [arrayToSearch objectEnumerator];
             while(pub = [pubEnum nextObject]){
                 accessorResult = [pub performSelector:NSSelectorFromString(selectorString) withObject:nil];
-                accessorResult = [[NSString lossyASCIIStringWithString:accessorResult] stringByRemovingTeX];
+                accessorResult = [[NSString lossyASCIIStringWithString:accessorResult] stringByRemovingCurlyBraces];
                 r = [accessorResult rangeOfString:componentSubstring
                                           options:NSCaseInsensitiveSearch];
                 if(r.location != NSNotFound){
@@ -1575,7 +1575,7 @@ stringByAppendingPathComponent:@"BibDesk"]; */
                 if(!value){
                     r.location = NSNotFound;
                 } else {
-                    value = [NSString lossyASCIIStringWithString:[value stringByRemovingTeX]];
+                    value = [NSString lossyASCIIStringWithString:[value stringByRemovingCurlyBraces]];
                     r = [value rangeOfString:componentSubstring
                                      options:NSCaseInsensitiveSearch];
                 }
@@ -1594,7 +1594,7 @@ stringByAppendingPathComponent:@"BibDesk"]; */
             pubEnum = [arrayToSearch objectEnumerator];
             while(pub = [pubEnum nextObject]){
                 accessorResult = [pub performSelector:NSSelectorFromString(selectorString) withObject:nil];
-                accessorResult = [NSString lossyASCIIStringWithString:accessorResult];
+                accessorResult = [[NSString lossyASCIIStringWithString:accessorResult] stringByRemovingCurlyBraces];
                 r = [accessorResult rangeOfString:componentSubstring
                                           options:NSCaseInsensitiveSearch];
                 if(r.location != NSNotFound){
