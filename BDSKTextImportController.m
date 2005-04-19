801e8caec54c2d440c365e5e76faf4ac8bcb1971
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-04-19 00:11:06 +0000

    Don't need to set Hidden. Works also for Jaguar. Connect WebFrameLoadDelegate.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@2109 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKTextImportController.m
+++ BDSKTextImportController.m
@@ -318,14 +318,10 @@
 #pragma mark WebFrameLoadDelegate
 
 - (void)webView:(WebView *)sender didStartProvisionalLoadForFrame:(WebFrame *)frame{
-	if ([progressIndicator respondsToSelector:@selector(setHidden:)])
-		[progressIndicator setHidden:NO];
 	[progressIndicator startAnimation:nil];
 }
 
 - (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame{
-	if ([progressIndicator respondsToSelector:@selector(setHidden:)])
-		[progressIndicator setHidden:YES];
 	[progressIndicator stopAnimation:nil];
 }
 
