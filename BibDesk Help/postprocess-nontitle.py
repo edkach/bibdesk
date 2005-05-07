56ab4c706749030573c79bcd614d100df3bfb022
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-05-07 13:10:11 +0000

    Change encoding to utf-8.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@2253 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibDesk Help/postprocess-nontitle.py
+++ BibDesk Help/postprocess-nontitle.py
@@ -11,21 +11,26 @@ for fname in fnames:
 
     teximetaline = 0
     metaline = 0
+    encodingline = 0
 
     for line in lines:
         if line[:34] == "<meta name=\"description\" content=\"":
             teximetaline = lines.index(line)
         if line[:24] == "<META NAME=\"DESCRIPTION\"":
             metaline = lines.index(line)
+        if line[:31] == "<meta http-equiv=\"Content-Type\"":
+            encodingline = lines.index(line)
+
+    f.close()
+    f = open(fname, 'w')
 
     if teximetaline > 0 and metaline > 0:
         lines[teximetaline] = lines[metaline]
         lines[metaline] = ""
+    if encodingline > 0:
+        lines[encodingline] = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n"
 
-        f.close()
-        f = open(fname, 'w')
-        f.seek(0)
-        for line in lines:
-            f.write(line)
-
-f.close()
+    f.seek(0)
+    for line in lines:
+        f.write(line)
+    f.close()
