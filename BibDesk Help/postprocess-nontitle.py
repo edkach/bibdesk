a38c648572443ab3ebef06aa7f474fdd1bfbd835
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-05-08 08:04:38 +0000

    Use normal encoding with special character for u-umlaut.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@2265 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibDesk Help/postprocess-nontitle.py
+++ BibDesk Help/postprocess-nontitle.py
@@ -11,26 +11,24 @@ for fname in fnames:
 
     teximetaline = 0
     metaline = 0
-    encodingline = 0
 
     for line in lines:
         if line[:34] == "<meta name=\"description\" content=\"":
             teximetaline = lines.index(line)
-        if line[:24] == "<META NAME=\"DESCRIPTION\"":
+        if line[:34] == "<META NAME=\"DESCRIPTION\" CONTENT=\"":
             metaline = lines.index(line)
-        if line[:31] == "<meta http-equiv=\"Content-Type\"":
-            encodingline = lines.index(line)
 
     f.close()
-    f = open(fname, 'w')
 
     if teximetaline > 0 and metaline > 0:
-        lines[teximetaline] = lines[metaline]
-        lines[metaline] = ""
-    if encodingline > 0:
-        lines[encodingline] = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n"
 
-    f.seek(0)
-    for line in lines:
-        f.write(line)
-    f.close()
+		f = open(fname, 'w')
+
+		lines[teximetaline] = lines[teximetaline][:34] + lines[metaline][34:]
+        lines[metaline] = ""
+
+		f.seek(0)
+        for line in lines:
+            f.write(line)
+
+		f.close()
