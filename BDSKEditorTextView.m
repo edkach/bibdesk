4e12d718383e3f6ad5a2b433afeb1e18c777b67f
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2006-03-01 23:01:36 +0000

    Some (hopefully) improvements for URL parsing.  Work in progress.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@5710 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKEditorTextView.m
+++ BDSKEditorTextView.m
@@ -7,6 +7,7 @@
 //
 
 #import "BDSKEditorTextView.h"
+#import "NSURL_BDSKExtensions.h"
 
 static NSString *BDSKEditorFontNameKey = @"BDSKEditorFontNameKey";
 static NSString *BDSKEditorFontSizeKey = @"BDSKEditorFontSizeKey";
@@ -50,18 +51,64 @@ static NSString *BDSKEditorFontSizeKey = @"BDSKEditorFontSizeKey";
     [[OFPreferenceWrapper sharedPreferenceWrapper] setFloat:[font pointSize] forKey:BDSKEditorFontSizeKey];
 }
 
-- (NSString *)editedWordFromRange:(NSRange *)editedRange inString:(NSString *)string
+static inline BOOL checkForPercentEscapeFromIndex(NSString *string, unsigned startIndex)
 {
-    NSRange range = [string rangeOfCharacterFromSet:[NSCharacterSet whitespaceAndNewlineCharacterSet] options:NSBackwardsSearch range:NSMakeRange(0, editedRange->location)];
+    NSCParameterAssert([string length] > startIndex);
+    // require % and two additional chars
+    if([string characterAtIndex:startIndex] != '%' || [string length] <= (startIndex + 2))
+        return NO;
+    
+    unichar ch1 = [string characterAtIndex:(startIndex + 1)];
+    unichar ch2 = [string characterAtIndex:(startIndex + 2)];
+    return ((ch1 <= '9' && ch1 >= '0') && (ch2 <= '9' && ch2 >= '0')) ? YES : NO;
+}
+
+// searches backwards; forwards search uses edited range as word boundary
+- (NSString *)URLStringFromRange:(NSRange *)startRange inString:(NSString *)string
+{
+    NSRange range;
+    NSRange searchRange = NSMakeRange(0, startRange->location);
+    
+    do {
+        range = [string rangeOfCharacterFromSet:[NSURL illegalURLCharacterSet] options:NSBackwardsSearch range:searchRange];
+        
+        // if we didn't find one, reset the range to the last useful value and break
+        if(range.location == NSNotFound){
+            range.location = searchRange.location;
+            break;
+        }
+           
+        // move the search range interval towards the beginning of the string
+        searchRange.length = range.location;
+           
+    } while (checkForPercentEscapeFromIndex(string, range.location));
+
     NSString *lastWord = nil;
     if(range.length){
-        // account for the whitespace character
-        range.location += 1;
         
-        // use the end of the edited range to determine the length
-        range.length = NSMaxRange(*editedRange) - range.location;
-        lastWord = [string substringWithRange:range];    
-        *editedRange = range;
+        // skip the illegal character
+        range.location += 1;
+
+        NSRange endRange;
+        searchRange = NSMakeRange(range.location, [string length] - range.location);
+        
+        do {
+            endRange = [string rangeOfCharacterFromSet:[NSURL illegalURLCharacterSet] options:0 range:searchRange];
+            
+            // if the entire string is valid...
+            if(endRange.location == NSNotFound){
+                endRange = NSMakeRange([string length], 0);
+                break;
+            }
+            
+            // move the search range interval towards the end of the string
+            searchRange = NSMakeRange(searchRange.location, endRange.location - searchRange.location);
+            
+        } while (checkForPercentEscapeFromIndex(string, endRange.location));
+        
+        range = NSMakeRange(range.location, endRange.location - range.location);
+        if(range.length) lastWord = [string substringWithRange:range];    
+        *startRange = range;
     }
     return lastWord;
 }
@@ -81,13 +128,7 @@ static NSString *BDSKEditorFontSizeKey = @"BDSKEditorFontSizeKey";
         range = [string rangeOfString:@"://" options:0 range:NSMakeRange(start, length - start)];
         
         if(range.length){
-            // get the next word boundary (use punctuation?)
-            wordRange = [string rangeOfCharacterFromSet:[NSCharacterSet whitespaceAndNewlineCharacterSet] options:0 range:NSMakeRange(range.location, length - range.location)];
-            wordRange.length -= 1; // lose the whitespace
-            wordRange.location = wordRange.location == NSNotFound ? length : wordRange.location;
-            
-            range = NSUnionRange(range, wordRange);
-            urlString = [self editedWordFromRange:&range inString:string];
+            urlString = [self URLStringFromRange:&range inString:string];
             url = urlString ? [[NSURL alloc] initWithString:urlString] : nil;
             if([url scheme]) [textStorage addAttribute:NSLinkAttributeName value:url range:range];
         }
@@ -105,8 +146,8 @@ static NSString *BDSKEditorFontSizeKey = @"BDSKEditorFontSizeKey";
     // if this is > 1, it's likely a paste or initial insertion, so fix the whole thing
     if(editedRange.length > 1){
         [self fixAttributesForURLs];
-    } else if(editedRange.length && editedRange.location != NSNotFound){
-        NSString *editedWord = [self editedWordFromRange:&editedRange inString:string];
+    } else if(editedRange.location != NSNotFound){
+        NSString *editedWord = [self URLStringFromRange:&editedRange inString:string];
         if([editedWord rangeOfString:@"://"].length == 0)
             editedWord = nil;
         NSURL *url = editedWord ? [[NSURL alloc] initWithString:editedWord] : nil;
@@ -115,6 +156,8 @@ static NSString *BDSKEditorFontSizeKey = @"BDSKEditorFontSizeKey";
         else
             [textStorage removeAttribute:NSLinkAttributeName range:editedRange];
         [url release];
+    } else {
+        NSLog(@"I am confused: edited range is %@", NSStringFromRange(editedRange));
     }
 }
 
