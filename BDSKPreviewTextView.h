41acbe75f518ac3998ade8f9b0df07740f2b6cd0
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2006-03-06 15:57:32 +0000

    Initial commit of files for new textView subclass used for preview pane.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@5761 0fceea05-a30d-0410-8a8b-80ef821fb0a1

---
+++ BDSKPreviewTextView.h
@@ -0,0 +1,16 @@
+//
+//  BDSKPreviewTextView.h
+//  Bibdesk
+//
+//  Created by Christiaan Hofman on 3/6/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface BDSKPreviewTextView : NSTextView {
+
+}
+
+@end
---
+++ BDSKPreviewTextView.m
@@ -0,0 +1,35 @@
+//
+//  BDSKPreviewTextView.m
+//  Bibdesk
+//
+//  Created by Christiaan Hofman on 3/6/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import "BDSKPreviewTextView.h"
+#import "BibPrefController.h"
+
+
+@implementation BDSKPreviewTextView
+
+- (void)updateFontPanel {
+    NSString *fontName = [[OFPreferenceWrapper sharedPreferenceWrapper] objectForKey:BDSKPreviewPaneFontFamilyKey];
+    float fontSize = [[OFPreferenceWrapper sharedPreferenceWrapper] floatForKey:BDSKPreviewPaneFontChangedNotification];
+	[[NSFontManager sharedFontManager] setSelectedFont:[NSFont fontWithName:fontName size:fontSize] isMultiple:NO];
+}
+
+- (void)changeFont:(id)sender {
+	NSFontManager *fontManager = [NSFontManager sharedFontManager];
+	NSFont *selectedFont = [fontManager selectedFont];
+	if (selectedFont == nil)
+		selectedFont = [NSFont systemFontOfSize:[NSFont systemFontSize]];
+	NSFont *font = [fontManager convertFont:selectedFont];
+    
+    [[OFPreferenceWrapper sharedPreferenceWrapper] setObject:[font familyName] forKey:BDSKPreviewPaneFontFamilyKey];
+    [[OFPreferenceWrapper sharedPreferenceWrapper] setFloat:[font pointSize] forKey:BDSKPreviewBaseFontSizeKey];
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:BDSKPreviewPaneFontChangedNotification object:nil];
+    [[NSNotificationCenter defaultCenter] postNotificationName:BDSKPreviewDisplayChangedNotification object:nil];
+}
+
+@end
