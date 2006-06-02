c68ce89b9c701ec53024a28d518765436490287b
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2006-06-02 14:31:01 +0000

    Change required filetype for export using template according to the chosen template.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@6792 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKTemplateParser.m
+++ BDSKTemplateParser.m
@@ -223,8 +223,7 @@ static NSCharacterSet *keyCharacterSet = nil;
                 @catch (id exception) { keyValue = nil; }
                 if (keyValue != nil) {
                     if ([keyValue isKindOfClass:[NSAttributedString class]]) {
-                        tmpAttrStr = [keyValue mutableCopy];
-                        [(NSMutableAttributedString *)tmpAttrStr addAttributes:attr range:NSMakeRange(0, [keyValue length])];
+                        tmpAttrStr = [[NSAttributedString alloc] initWithAttributedString:keyValue attributes:attr];
                     } else {
                         tmpAttrStr = [[NSAttributedString alloc] initWithString:[keyValue stringDescription] attributes:attr];
                     }
@@ -396,6 +395,29 @@ static NSCharacterSet *keyCharacterSet = nil;
             wsRange = NSMakeRange(rangeEnd, end - rangeEnd);
     }
     return wsRange;
+}
+
+@end
+
+
+@implementation NSAttributedString (BDSKTemplateParser)
+
+- (id)initWithAttributedString:(NSAttributedString *)attributedString attributes:(NSDictionary *)attributes {
+    [[self init] release];
+    NSMutableAttributedString *tmpStr = [[NSMutableAttributedString alloc] initWithAttributedString:attributedString];
+    unsigned index = 0, length = [attributedString length];
+    NSRange range = NSMakeRange(0, length);
+    NSDictionary *attrs;
+    [tmpStr addAttributes:attributes range:range];
+    while (index < length) {
+        attrs = [attributedString attributesAtIndex:index effectiveRange:&range];
+        if (range.length > 0) {
+            [tmpStr addAttributes:attrs range:range];
+            index = NSMaxRange(range);
+        } else index++;
+    }
+    [tmpStr fixAttributesInRange:NSMakeRange(0, [self length])];
+    return self = tmpStr;
 }
 
 @end
