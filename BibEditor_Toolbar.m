1c9c3e9f96d13fab4183d8270af12df649320a14
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2006-08-27 12:32:25 +0000

    Copy toolbar item views for the customization palette.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@7582 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibEditor_Toolbar.m
+++ BibEditor_Toolbar.m
@@ -207,7 +207,7 @@ NSString *BibEditorToolbarAddWithCrossrefItemIdentifier = @"BibEditorToolbarAddW
     OAToolbarItem *item = [toolbarItems objectForKey:itemIdent];
     OAToolbarItem *newItem = [[item copy] autorelease];
     // the view should not be copied
-    if ([item view]) [newItem setView:[item view]];
+    if ([item view] && willBeInserted) [newItem setView:[item view]];
     return newItem;
 }
 
