3c73df4cf007a3857625180aacfe48f341f9db2e
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2006-11-13 08:08:15 +0000

    Optimize the TeX command parsing by operating on NSMutableStrings as much as possible instead of the attributed string.  Also optimize for the common case of no font commands.  Add comments.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@8421 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- NSAttributedString_BDSKExtensions.m
+++ NSAttributedString_BDSKExtensions.m
@@ -42,26 +42,13 @@
 #import "NSCharacterSet_BDSKExtensions.h"
 #import "BDSKFontManager.h"
 
+static NSString *__rangeKey = @"__BDSKRange";
 
-@implementation NSAttributedString (BDSKExtensions)
-
-- (id)initWithTeXString:(NSString *)string attributes:(NSDictionary *)attributes collapseWhitespace:(BOOL)collapse{
-    [[self init] release];
-    
-    // get rid of whitespace if we have to; we can't use this on the attributed string's content store, though
-    if(collapse){
-        if([string isComplex])
-            string = [NSString stringWithString:string];
-        string = [string fastStringByCollapsingWhitespaceAndRemovingSurroundingWhitespace];
-    }
-    
-    // set up the attributed string now, so we can start working with its character contents
-    NSMutableAttributedString *mas = [[NSMutableAttributedString alloc] initWithString:string attributes:attributes]; // set the whole thing up with default attrs
-    NSMutableString *mutableString = [mas mutableString];
-    
+static NSArray *copyAttributeDictionariesAndFixString(NSMutableString *mutableString, NSDictionary *attributes)
+{
     NSFontManager *fontManager = [NSFontManager sharedFontManager];
     NSString *texStyle = nil;    
-    NSMutableDictionary *attrs = [attributes mutableCopy];
+    NSMutableDictionary *attrs;
     NSFont *font = [attributes objectForKey:NSFontAttributeName];
     if (font == nil)
         font = [NSFont systemFontOfSize:0];
@@ -72,32 +59,107 @@
     unsigned startLoc; // starting character index to apply tex attributes
     unsigned endLoc;   // ending index to apply tex attributes
     
+    NSMutableArray *attributeDictionaries = [[NSMutableArray alloc] init];
+    CFAllocatorRef alloc = CFGetAllocator(mutableString);
+    
     while( (cmdRange = [mutableString rangeOfTeXCommandInRange:searchRange]).location != NSNotFound){
         
-        // find the command
-        texStyle = [mutableString substringWithRange:cmdRange];
+        // copy the command
+        texStyle = (NSString *)CFStringCreateWithSubstring(alloc, (CFStringRef)mutableString, CFRangeMake(cmdRange.location, cmdRange.length));
         
         // delete the command, now that we know what it was
         [mutableString deleteCharactersInRange:cmdRange];
         
-        // what does the command affect?
-        startLoc = cmdRange.location;  // remember, we deleted our command, but not the brace
-        if([mutableString characterAtIndex:startLoc] == '{' && (endLoc = [mutableString indexOfRightBraceMatchingLeftBraceAtIndex:startLoc]) != NSNotFound){
-            [attrs setObject:[fontManager convertFont:font toHaveTrait:[fontManager fontTraitMaskForTeXStyle:texStyle]]
-                      forKey:NSFontAttributeName];
-            styleRange = NSMakeRange(startLoc + 1, (endLoc - startLoc - 1));
-            //NSLog(@"applying to %@", [mutableString substringWithRange:styleRange]);
-            [mas setAttributes:attrs range:styleRange];
+        startLoc = cmdRange.location;
+        
+        // see if this is a font command
+        NSFontTraitMask newTrait = [fontManager fontTraitMaskForTeXStyle:texStyle];
+        [texStyle release];
+        
+        if (0 != newTrait) {
+            
+            // remember, we deleted our command, but not the brace
+            if([mutableString characterAtIndex:startLoc] == '{' && (endLoc = [mutableString indexOfRightBraceMatchingLeftBraceAtIndex:startLoc]) != NSNotFound){
+                
+                // have to delete the braces as we go along, or else ranges will be hosed after deleting at the end
+                [mutableString deleteCharactersInRange:NSMakeRange(startLoc, 1)];
+                
+                // deleting the left brace just shifted everything to the left
+                [mutableString deleteCharactersInRange:NSMakeRange(endLoc - 1, 1)];
+                
+                attrs = [attributes mutableCopy];
+                [attrs setObject:[fontManager convertFont:font toHaveTrait:newTrait]
+                          forKey:NSFontAttributeName];
+                
+                // account for the braces, since we'll be removing them
+                styleRange = NSMakeRange(startLoc, (endLoc - startLoc - 1));
+                
+                [attrs setObject:[NSValue valueWithRange:styleRange] forKey:__rangeKey];
+                [attributeDictionaries addObject:attrs];
+                [attrs release];
+            }
         }
-        // new range, since we've altered the string
+        // new range, since we've altered the string (we don't use endLoc because of possibly nested commands)
         searchRange = NSMakeRange(startLoc, [mutableString length] - startLoc);
     }
-
-    [attrs release];
-    [mutableString deleteCharactersInCharacterSet:[NSCharacterSet curlyBraceCharacterSet]];
     
-    self = [mas copy];
-    [mas release];
+    return attributeDictionaries;
+}
+
+static void applyAttributesToString(const void *value, void *context)
+{
+    NSDictionary *dict = (void *)value;
+    NSMutableAttributedString *mas = context;
+    [mas addAttributes:dict range:[[dict objectForKey:__rangeKey] rangeValue]];    
+}
+
+
+@implementation NSAttributedString (BDSKExtensions)
+
+- (id)initWithTeXString:(NSString *)string attributes:(NSDictionary *)attributes collapseWhitespace:(BOOL)collapse{
+
+    NSMutableAttributedString *mas;
+    
+    // get rid of whitespace if we have to; we can't use this on the attributed string's content store, though
+    if(collapse){
+        if([string isComplex])
+            string = [NSString stringWithString:string];
+        string = [string fastStringByCollapsingWhitespaceAndRemovingSurroundingWhitespace];
+    }
+    
+    NSMutableString *mutableString = [string mutableCopy];
+    
+    // Parse the TeX commands and remove them from the string, manipulating the NSMutableString as much as possible, since -[NSMutableAttributedString mutableString] returns a proxy object that's more expensive.
+    NSArray *attributeDictionaries = copyAttributeDictionariesAndFixString(mutableString, attributes);
+    
+    unsigned numberOfDictionaries = [attributeDictionaries count];
+    if (numberOfDictionaries > 0) {
+
+        // discard the result of +alloc, since we're going to create a new object
+        [[self init] release];
+
+        // set the attributed string up with default attributes, after parsing and fixing the mutable string
+        mas = [[NSMutableAttributedString alloc] initWithString:mutableString attributes:attributes]; 
+        [mutableString release];
+
+        // now apply the previously determined attributes and ranges to the attributed string
+        CFArrayApplyFunction((CFArrayRef)attributeDictionaries, CFRangeMake(0, numberOfDictionaries), applyAttributesToString, mas);
+        
+        // not all of the braces were deleted when parsing the commands
+        [[mas mutableString] deleteCharactersInCharacterSet:[NSCharacterSet curlyBraceCharacterSet]];
+        
+        self = [mas copy];
+        [mas release];
+        
+    } else {
+        
+        // no font commands, so operate directly on the NSMutableString and then use the result of +alloc
+        [mutableString deleteCharactersInCharacterSet:[NSCharacterSet curlyBraceCharacterSet]];
+        self = [self initWithString:mutableString attributes:attributes];
+    }
+    
+    [mutableString release];
+    [attributeDictionaries release];
     
     return self;
 }
