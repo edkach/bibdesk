1b5ed17b948ebd63558556787878c2e3e0419bf4
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-03-29 17:42:27 +0000

    Return nil if there are multiple matches to the pubmed query.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@10365 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibItem_PubMedLookup.m
+++ BibItem_PubMedLookup.m
@@ -16,14 +16,10 @@
 
 @implementation BibItem (PubMedLookup)
 
-// max number of results from NCBI is 100, except on evenings and weekends
-#define MAX_RESULTS 50
-
 /* Based on public domain sample code written by Oleg Khovayko, available at
  http://www.ncbi.nlm.nih.gov/entrez/query/static/eutils_example.pl
  
- - We limit requests to 100 in the editor interface, per NCBI's request.  
- - We also pass tool=bibdesk for their tracking purposes.  
+ - We pass tool=bibdesk for their tracking purposes.  
  - We use lower case characters in the URL /except/ for WebEnv
  - See http://www.ncbi.nlm.nih.gov/entrez/query/static/eutils_help.html for details.
  
@@ -41,7 +37,6 @@
 
 + (NSString *)baseURLString { return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils"; }
 
-    // may be useful for UI validation
 + (BOOL)canConnect;
 {
     CFURLRef theURL = (CFURLRef)[NSURL URLWithString:[self baseURLString]];
@@ -70,19 +65,17 @@
         
     NSXMLDocument *document = nil;
     
-    // get the initial XML document with our search parameters in it; we're only asking for a single result
-    NSString *esearch = [[[self class] baseURLString] stringByAppendingFormat:@"/esearch.fcgi?db=pubmed&retmax=1&usehistory=y&term=%@&tool=bibdesk", pmid];
-    NSURL *initialURL = [NSURL URLWithString:esearch]; 
-    OBPRECONDITION(initialURL);
+    // get the initial XML document with our search parameters in it; we ask for 2 results at most
+    NSString *esearch = [[[self class] baseURLString] stringByAppendingFormat:@"/esearch.fcgi?db=pubmed&retmax=2&usehistory=y&term=%@&tool=bibdesk", pmid];
+    NSURL *theURL = [NSURL URLWithString:esearch]; 
+    OBPRECONDITION(theURL);
     
-    NSURLRequest *request = [NSURLRequest requestWithURL:initialURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:1.0];
+    NSURLRequest *request = [NSURLRequest requestWithURL:theURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:1.0];
     NSURLResponse *response;
     NSError *error;
     NSData *esearchResult = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
     
-    if (nil == esearchResult)
-        NSLog(@"failed to download %@ with error %@", initialURL, error);
-    else
+    if ([esearchResult length])
         document = [[NSXMLDocument alloc] initWithData:esearchResult options:NSXMLNodeOptionsNone error:&error];
     
     if (nil != document) {
@@ -91,36 +84,37 @@
         // we need to extract WebEnv, Count, and QueryKey to construct our final URL
         NSString *webEnv = [[[root nodesForXPath:@"/eSearchResult[1]/WebEnv[1]" error:NULL] lastObject] stringValue];
         NSString *queryKey = [[[root nodesForXPath:@"/eSearchResult[1]/QueryKey[1]" error:NULL] lastObject] stringValue];
-        
-        // get the first result (zero-based indexing)
-        NSString *efetch = [[[self class] baseURLString] stringByAppendingFormat:@"/efetch.fcgi?rettype=medline&retmode=text&retstart=0&retmax=1&db=pubmed&query_key=%@&WebEnv=%@&tool=bibdesk", queryKey, webEnv];
-        NSURL *theURL = [NSURL URLWithString:efetch];
-        OBPOSTCONDITION(theURL);
-        [document release];
-        
-        request = [NSURLRequest requestWithURL:theURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:1.0];
-        NSData *efetchResult = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-        
-        if (efetchResult) {
-            
-            // try to get encoding from the http headers; returned nil when I tried
-            NSString *encodingName = [response textEncodingName];
-            NSStringEncoding encoding = encodingName ? CFStringConvertEncodingToNSStringEncoding(CFStringConvertIANACharSetNameToEncoding((CFStringRef)encodingName)) : kCFStringEncodingInvalidId;
-            
-            if (encoding != kCFStringEncodingInvalidId)
-                toReturn = [[NSString alloc] initWithData:efetchResult encoding:encoding];
-            else
-                toReturn = [[NSString alloc] initWithData:efetchResult encoding:NSUTF8StringEncoding];
-            
-            if (nil == toReturn)
-                toReturn = [[NSString alloc] initWithData:efetchResult encoding:NSISOLatin1StringEncoding];
-            
-            [toReturn autorelease];
-        }
+        id count = [[[root nodesForXPath:@"/eSearchResult[1]/Count[1]" error:NULL] lastObject] objectValue];
 
-    } else if (nil != esearchResult) {
-        // make sure error was actually initialized by NSXMLDocument
-        NSLog(@"unable to create an XML document: %@", error);
+        // ensure that we only have a single result; if it's ambiguous, just return nil
+        if ([count intValue] == 1) {  
+            
+            // get the first result (zero-based indexing)
+            NSString *efetch = [[[self class] baseURLString] stringByAppendingFormat:@"/efetch.fcgi?rettype=medline&retmode=text&retstart=0&retmax=1&db=pubmed&query_key=%@&WebEnv=%@&tool=bibdesk", queryKey, webEnv];
+            theURL = [NSURL URLWithString:efetch];
+            OBPOSTCONDITION(theURL);
+            [document release];
+            
+            request = [NSURLRequest requestWithURL:theURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:1.0];
+            NSData *efetchResult = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+            
+            if (efetchResult) {
+                
+                // try to get encoding from the http headers; returned nil when I tried
+                NSString *encodingName = [response textEncodingName];
+                NSStringEncoding encoding = encodingName ? CFStringConvertEncodingToNSStringEncoding(CFStringConvertIANACharSetNameToEncoding((CFStringRef)encodingName)) : kCFStringEncodingInvalidId;
+                
+                if (encoding != kCFStringEncodingInvalidId)
+                    toReturn = [[NSString alloc] initWithData:efetchResult encoding:encoding];
+                else
+                    toReturn = [[NSString alloc] initWithData:efetchResult encoding:NSUTF8StringEncoding];
+                
+                if (nil == toReturn)
+                    toReturn = [[NSString alloc] initWithData:efetchResult encoding:NSISOLatin1StringEncoding];
+                
+                [toReturn autorelease];
+            }
+        }
     }
     
     return toReturn;
