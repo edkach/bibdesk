a984101b7c4b6521d11e6ebc47be1f5e5fb41254
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-04-12 20:12:41 +0000

    - Update for new agent protocol name. - Refactor some code, and add debug version that logs to stderr. - Register for connection death, so killing the agent will reset the client. - Add a sanity check for connection failures. - Keep the proxy as an ivar until the connection dies, so we don't have to   go through the exception handler on every call. - Launch task in an exception handler in case it fails.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@10445 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKSkimReader.m
+++ BDSKSkimReader.m
@@ -3,13 +3,43 @@
 //  Bibdesk
 //
 //  Created by Adam Maxwell on 04/09/07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
-//
+/*
+ This software is Copyright (c) 2007
+ Adam Maxwell. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ - Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ - Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in
+ the documentation and/or other materials provided with the
+ distribution.
+ 
+ - Neither the name of Adam Maxwell nor the names of any
+ contributors may be used to endorse or promote products derived
+ from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "BDSKSkimReader.h"
 #import "NSWorkspace_BDSKExtensions.h"
 
-@protocol ListenerProtocol
+@protocol SKAgentListenerProtocol
 
 - (bycopy NSData *)RTFNotesAtPath:(in bycopy NSString *)aFile;
 - (bycopy NSString *)textNotesAtPath:(in bycopy NSString *)aFile;
@@ -30,34 +60,11 @@
     return sharedInstance;
 }
 
-- (void)establishConnection;
-{
-    // okay to launch multiple instances, since the new one will just die
-    NSTask *task = [[NSTask alloc] init];
-    [task setLaunchPath:[[NSBundle mainBundle] pathForResource:@"SkimNotesAgent" ofType:nil]];
-    [task setArguments:[NSArray arrayWithObject:AGENT_IDENTIFIER]];
-    
-    // task will print the identifier to standard output; we don't care about it, since we specified it
-    [task setStandardOutput:[NSFileHandle fileHandleWithNullDevice]];
-    [task launch];
-    [task release];
-    
-    int maxTries = 5;
-    connection = [[NSConnection connectionWithRegisteredName:AGENT_IDENTIFIER host:nil] retain];
-
-    // if we try to read data before the server is fully set up, connection will still be nil
-    while (nil == connection && maxTries--) { 
-        [NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-        connection = [[NSConnection connectionWithRegisteredName:AGENT_IDENTIFIER host:nil] retain];
-    }
-    
-    // if we don't set these explicitly, timeout never seems to take place
-    [connection setRequestTimeout:AGENT_TIMEOUT];
-    [connection setReplyTimeout:AGENT_TIMEOUT];
-}    
-
 - (void)destroyConnection;
 {
+    [agent release];
+    agent = nil;
+    
     [[connection receivePort] invalidate];
     [[connection sendPort] invalidate];
     [connection invalidate];
@@ -67,9 +74,101 @@
 
 - (void)dealloc
 {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     [self destroyConnection];
     [super dealloc];
 }
+
+- (void)handleConnectionDied:(NSNotification *)note;
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:NSConnectionDidDieNotification object:[note object]];
+    // ensure the proxy ivar and ports are cleaned up; is it still okay to message it?
+    [self destroyConnection];
+}
+
+- (BOOL)launchedTask;
+{
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath:[[NSBundle mainBundle] pathForResource:@"SkimNotesAgent" ofType:nil]];
+    [task setArguments:[NSArray arrayWithObject:AGENT_IDENTIFIER]];
+    BOOL taskLaunched = NO;
+    
+#if OMNI_FORCE_ASSERTIONS
+    [task setStandardError:[NSFileHandle fileHandleWithStandardError]];
+    NSPipe *pipe = [NSPipe pipe];
+    [task setStandardOutput:pipe];
+    @try {
+        [task launch];
+        NSData *data = [[pipe fileHandleForReading] readDataToEndOfFile];
+        NSLog(@"SkimNotesAgent started with identifier \"%@\"", [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease]);
+        taskLaunched = [task isRunning];
+    }
+#else
+    // task will print the identifier to standard output; we don't care about it, since we specified it
+    [task setStandardOutput:[NSFileHandle fileHandleWithNullDevice]];
+    [task setStandardError:[NSFileHandle fileHandleWithNullDevice]];
+    @try {
+        [task launch];
+        taskLaunched = [task isRunning];
+    }
+#endif
+    
+    @catch(id exception){
+        NSLog(@"failed to launch SkimNotesAgent: %@", exception);
+        taskLaunched = NO;
+    }
+    [task release];
+    return taskLaunched;
+}    
+
+- (void)establishConnection;
+{
+    static int numberOfConnectionAttempts = 0;
+    if (numberOfConnectionAttempts++ > 100) {
+        static BOOL didWarn = NO;
+        if (NO == didWarn) {
+            NSLog(@"*** Insane number of Skim agent connection failures; disabling further attempts ***");
+            didWarn = YES;
+        }
+        return;
+    }
+    
+    // okay to launch multiple instances, since the new one will just die, but we generally shouldn't do that
+    OBPRECONDITION(nil == connection);
+
+    // no point in trying to connect if the task didn't launch
+    if ([self launchedTask]) {
+        int maxTries = 5;
+        connection = [[NSConnection connectionWithRegisteredName:AGENT_IDENTIFIER host:nil] retain];
+
+        // if we try to read data before the server is fully set up, connection will still be nil
+        while (nil == connection && maxTries--) { 
+            [NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+            connection = [[NSConnection connectionWithRegisteredName:AGENT_IDENTIFIER host:nil] retain];
+        }
+        
+        if (connection) {
+            
+            // keep an eye on the connection from our end, so we can retain the proxy object
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleConnectionDied:) name:NSConnectionDidDieNotification object:connection];
+        
+            // if we don't set these explicitly, timeout never seems to take place
+            [connection setRequestTimeout:AGENT_TIMEOUT];
+            [connection setReplyTimeout:AGENT_TIMEOUT];
+            
+            @try {
+                id server = [connection rootProxy];
+                [server setProtocolForProxy:@protocol(SKAgentListenerProtocol)];
+                agent = [server retain];
+            }
+            @catch(id exception) {
+                NSLog(@"Discarding exception %@ caught when contacting SkimNotesAgent", exception);
+                [self destroyConnection];
+            }
+        }
+    }
+    OBPOSTCONDITION(nil != connection);
+}    
 
 - (BOOL)connectAndCheckTypeOfFile:(NSURL *)fileURL;
 {
@@ -81,7 +180,7 @@
     if ([[NSFileManager defaultManager] fileExistsAtPath:[fileURL path] isDirectory:&isDir] == NO || isDir)
         return NO;
     
-    // make sure it's a PDF file before hitting the server...
+    // make sure it's a PDF file before hitting the agent...
     if (UTTypeConformsTo((CFStringRef)[[NSWorkspace sharedWorkspace] UTIForURL:fileURL], kUTTypePDF) == FALSE)
         return NO;
     return YES;
@@ -91,16 +190,7 @@
 {   
     NSData *data = nil;
     if ([self connectAndCheckTypeOfFile:fileURL]) {
-        @try {
-            id server = [connection rootProxy];
-            [server setProtocolForProxy:@protocol(ListenerProtocol)];
-            data = [server RTFNotesAtPath:[fileURL path]];
-        }
-        @catch(id exception) {
-            NSLog(@"Discarding exception %@ caught when contacting SkimNotesAgent", exception);
-            data = nil;
-            [self destroyConnection];
-        }
+        data = [agent RTFNotesAtPath:[fileURL path]];
     }
     return data;
 }
@@ -110,19 +200,10 @@
     NSData *RTF = [self RTFNotesAtURL:fileURL];
     return RTF ? [[[[NSAttributedString alloc] initWithRTF:RTF documentAttributes:NULL] autorelease] string] : nil;
     
-    // @@ fixme: not functional on the skim side; worth it? to add a text version of notes in EA?
+    // @@ fixme: not functional on the skim side; worth it to add a text version of notes in EA?
     NSString *string = nil;
     if ([self connectAndCheckTypeOfFile:fileURL]) {
-        @try {
-            id server = [connection rootProxy];
-            [server setProtocolForProxy:@protocol(ListenerProtocol)];
-            string = [server textNotesAtPath:[fileURL path]];
-        }
-        @catch(id exception) {
-            NSLog(@"Discarding exception %@ caught when contacting SkimNotesAgent", exception);
-            string = nil;
-            [self destroyConnection];
-        }
+        string = [agent textNotesAtPath:[fileURL path]];
     }
     return string;
 }
