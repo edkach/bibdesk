89dfd85d3f465abd47c9998b2d66f70ee6033221
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-05-10 15:34:11 +0000

    Add Auto to localizable zoom values.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@10601 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKZoomablePDFView.m
+++ BDSKZoomablePDFView.m
@@ -108,6 +108,7 @@ static CFMutableSetRef nonretainedScrollviews = NULL;
 @implementation BDSKZoomablePDFView
 
 /* For genstrings:
+    NSLocalizedStringFromTable(@"Auto", @"ZoomValues", @"Zoom popup entry")
     NSLocalizedStringFromTable(@"10%", @"ZoomValues", @"Zoom popup entry")
     NSLocalizedStringFromTable(@"25%", @"ZoomValues", @"Zoom popup entry")
     NSLocalizedStringFromTable(@"50%", @"ZoomValues", @"Zoom popup entry")
