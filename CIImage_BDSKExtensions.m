1799281fe002c3c7860168084d35595ea1e5ace0
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-11-01 10:06:13 +0000

    Partly revert changes for Leopard gradients. Change is not needed as long as we compile on 10.4.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@11417 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- CIImage_BDSKExtensions.m
+++ CIImage_BDSKExtensions.m
@@ -46,14 +46,11 @@ static NSString *endColorKey = nil;
 
 + (void)initialize {
     OBINITIALIZE;
-    if (floor(NSAppKitVersionNumber) <= 824) {
-        // in Tiger the input colors are reversed
-        startColorKey = [@"inputColor1" retain];
-        endColorKey = [@"inputColor0" retain];
-    } else {
-        startColorKey = [@"inputColor0" retain];
-        endColorKey = [@"inputColor1" retain];
-    }
+    
+    // @@ Leopard: change this when compiling against 10.5 SDK
+    // in Tiger the input colors are reversed
+    startColorKey = [@"inputColor1" retain];
+    endColorKey = [@"inputColor0" retain];
 }
 
 + (CIImage *)imageWithConstantColor:(CIColor *)color;
