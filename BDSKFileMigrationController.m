3dbff22f63ac954d287d58e47b151565880efebe
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-12-18 15:17:07 +0000

    Add a flag to use selected pubs for file migration. Set window frame autosave name programmatically, as doing this in the nib is buggy. Add license info.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@11955 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKFileMigrationController.m
+++ BDSKFileMigrationController.m
@@ -3,14 +3,49 @@
 //  Bibdesk
 //
 //  Created by Adam Maxwell on 12/16/07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
-//
+/*
+ This software is Copyright (c) 2007
+ Adam Maxwell. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ - Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ - Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in
+ the documentation and/or other materials provided with the
+ distribution.
+ 
+ - Neither the name of Adam Maxwell nor the names of any
+ contributors may be used to endorse or promote products derived
+ from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "BDSKFileMigrationController.h"
 #import "BibDocument.h"
 #import "BibDocument_Actions.h"
+#import "BibDocument_Groups.h"
+#import "BDSKPublicationsArray.h"
 #import "BibItem.h"
 #import "BDSKLinkedFile.h"
+#import "NSWindowController_BDSKExtensions.h"
+
+static NSString *BDSKFileMigrationFrameAutosaveName = @"BDSKFileMigrationWindow";
 
 @interface BDSKURLTransformer : NSValueTransformer
 @end
@@ -36,6 +71,7 @@
     if (self) {
         results = [NSMutableArray new];
         keepOriginalValues = YES;
+        useSelection = NO;
     }
     return self;
 }
@@ -48,6 +84,7 @@
 
 - (void)awakeFromNib
 {
+    [self setWindowFrameAutosaveNameOrCascade:BDSKFileMigrationFrameAutosaveName];
     [tableView setDoubleAction:@selector(editPublication:)];
     [tableView setTarget:self];
     [tableView setDataSource:self];
@@ -80,15 +117,19 @@
     // get rid of leftovers from a previous run
     [[self mutableArrayValueForKey:@"results"] removeAllObjects];
     
-    BDSKPublicationsArray *pubs = [[self document] publications];
-    NSEnumerator *pubEnum = [pubs objectEnumerator];
-    BibItem *aPub;
+    NSArray *pubs = nil;
+    if (useSelection == NO)
+        pubs = [[self document] publications];
+    else if ([[self document] hasExternalGroupsSelected] == NO)
+        pubs = [[self document] selectedPublications];
     
     [progressBar setDoubleValue:0.0];
     [progressBar setHidden:NO];
     
     double current = 0, final = [pubs count];
     unsigned update = 0;
+    NSEnumerator *pubEnum = [pubs objectEnumerator];
+    BibItem *aPub;
     
     while (aPub = [pubEnum nextObject]) {
         NSError *error;
