544f091f096b9cc9806971834f1c689000f27b56
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-12-19 17:08:39 +0000

    Check total number of added files.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@11976 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKFileMigrationController.m
+++ BDSKFileMigrationController.m
@@ -173,7 +173,7 @@ static NSString *BDSKFileMigrationFrameAutosaveName = @"BDSKFileMigrationWindow"
     [statusField setStringValue:[NSString stringWithFormat:messageFormat, numberOfAddedFiles, numberOfRemovedFields]];
     
     // BibItem change notifications are only posted if the old fields are removed, so this ensures that the file view is updated
-    if (addedFiles > 0)
+    if (numberOfAddedFiles > 0)
         [[self document] updatePreviews];
 }
 
