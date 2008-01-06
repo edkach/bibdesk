1a503b809fe71e57ee909ea1175864f91b611db1
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2008-01-06 06:27:59 +0000

    Change copyrights to use dash-separated years, as Apple does. This gets rid of some unwieldy lines.
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@12305 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKMacroEditor.h
+++ BDSKMacroEditor.h
@@ -5,7 +5,7 @@
 // lost record of.
 
 /*
- This software is Copyright (c) 2005,2007,2008
+ This software is Copyright (c) 2005-2008
  Michael O. McCracken. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
