e2e38ef488b0a5ff950f2b24203bac99fe933909
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2008-10-31 11:14:06 +0000

    fix typo
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@13786 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKZoomablePDFView.m
+++ BDSKZoomablePDFView.m
@@ -378,7 +378,7 @@ static float BDSKScaleMenuFontSize = 11.0;
     if (fabsf(pinchZoomFactor - 1.0) > 0.1)
         [self setScaleFactor:fmaxf(pinchZoomFactor * [self scaleFactor], BDSKDefaultScaleMenuFactors[1])];
     if (fabsf(gestureRotation) > 45.0) {
-        [[self currentPage] setRotation:[[self currentPage] rotation] + (int)(90.0 * froundf(gestureRotation / 90.0))];
+        [[self currentPage] setRotation:[[self currentPage] rotation] + (int)(90.0 * roundf(gestureRotation / 90.0))];
         [self setNeedsDisplay:YES];
     }
     pinchZoomFactor = 1.0;
