b669b7cba317bcb0d6d68bb2051f3d023b1c408a
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2008-12-11 18:13:49 +0000

    get attributes from start of template tag
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@13901 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKTemplateParser.m
+++ BDSKTemplateParser.m
@@ -549,7 +549,7 @@ static inline NSRange rangeAfterRemovingEmptyLines(NSString *string, BDSKTemplat
         
         if ([scanner scanString:START_TAG_OPEN_DELIM intoString:nil]) {
             
-            attr = [template attributesAtIndex:[scanner scanLocation] - 1 effectiveRange:NULL];
+            attr = [template attributesAtIndex:[scanner scanLocation] - [START_TAG_OPEN_DELIM length] effectiveRange:NULL];
             start = [scanner scanLocation];
             
             // scan the key, must be letters and dots. We don't allow extra spaces
