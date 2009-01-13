d4a981ab65bcc819eb77e39df55976e49887799c
jefferis <jefferis@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2009-01-13 12:31:09 +0000

    Added basic unit tests for BDSKRISParser
    - checks canParseString and contentStringType
    - and also checks a miminimalBibTex equivalent of a record.
    - nb complains if a record with single spaced separated field names
      ie
      	TY - Some text
      rather than
    	TY  - Some text
      parses successfully.
    
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@14015 0fceea05-a30d-0410-8a8b-80ef821fb0a1

---
+++ UnitTests/TestBDSKRISParser.m
@@ -0,0 +1,56 @@
+//
+//  TestBDSKRISParser.m
+//  Bibdesk
+//
+//  Created by Gregory Jefferis on 2009-01-12.
+//  Copyright 2009 Gregory Jefferis. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "BibItem.h"
+#import "BDSKStringConstants.h"
+#import "BDSKRISParser.h"
+
+
+static NSString *goodRIS = @"TY  - JOUR\nT1  - Julian Steward, American Anthropology, and Colonialism\nA1  - Marc Pinkoski\nJF  - Histories of Anthropology Annual\nVL  - 4\nSP  - 172\nEP  - 204\nY1  - 2008\nPB  - University of Nebraska Press\nSN  - 1940-5138\nUR  - http://muse.jhu.edu/journals/histories_of_anthropology_annual/v004/4.pinkoski.html\nN1  - Volume 4, 2008\nER  - \n";
+static NSString *goodRISNoFinalReturnOrSpace = @"TY  - JOUR\nT1  - Julian Steward, American Anthropology, and Colonialism\nA1  - Marc Pinkoski\nJF  - Histories of Anthropology Annual\nVL  - 4\nSP  - 172\nEP  - 204\nY1  - 2008\nPB  - University of Nebraska Press\nSN  - 1940-5138\nUR  - http://muse.jhu.edu/journals/histories_of_anthropology_annual/v004/4.pinkoski.html\nN1  - Volume 4, 2008\nER  -";
+static NSString *badRISSingleSpace = @"TY - JOUR\nT1 - Julian Steward, American Anthropology, and Colonialism\nA1 - Marc Pinkoski\nJF - Histories of Anthropology Annual\nVL - 4\nSP - 172\nEP - 204\nY1 - 2008\nPB - University of Nebraska Press\nSN - 1940-5138\nUR - http://muse.jhu.edu/journals/histories_of_anthropology_annual/v004/4.pinkoski.html\nN1 - Volume 4, 2008\nER -\n";
+
+@interface TestBDSKRISParser : SenTestCase {
+}
+@end
+
+@implementation TestBDSKRISParser
+- (void)testCanParseString{
+	
+    STAssertEquals([BDSKStringParser canParseString:goodRIS ofType:BDSKRISStringType ],
+				   YES, @"Check that we can parse a basic RIS record");
+    STAssertEquals([BDSKStringParser canParseString:goodRIS ofType:BDSKRISStringType ],
+				   YES, @"Check that we can parse a basic RIS record even with missing final return");
+    STAssertEquals([BDSKStringParser canParseString:badRISSingleSpace ofType:BDSKRISStringType ],
+				   NO, @"Check that we reject a RIS record with a missing space in front of dash");
+    // GJ: Maybe it would be nice to make this possible some day ...
+	STAssertEquals([BDSKStringParser canParseString:goodRIS],
+				   NO, @"Can't parse without type information");
+}
+
+- (void)testContentStringType{
+	STAssertEquals(BDSKRISStringType, [goodRIS contentStringType],@"Check that this string is recognised as RIS content");
+	STAssertEquals(BDSKUnknownStringType, [badRISSingleSpace contentStringType],@"Check that this string is not recognised as RIS content");
+}
+
+- (void)testRISToMinimalBibTex{
+	BibItem *b = [[BDSKStringParser itemsFromString:goodRIS error:NULL] lastObject];
+	BibItem *b2 = [[BDSKStringParser itemsFromString:goodRISNoFinalReturnOrSpace error:NULL] lastObject];
+	
+	// These are fairly broad spectrum tests - would probably be better to break it down some more.
+	
+	STAssertEqualObjects([b bibTeXStringWithOptions:BDSKBibTeXOptionDropInternalMask],
+						 @"@article{cite-key,\n\tAuthor = {Marc Pinkoski},\n\tJournal = {Histories of Anthropology Annual},\n\tPages = {172--204},\n\tTitle = {Julian Steward, American Anthropology, and Colonialism},\n\tVolume = {4},\n\tYear = {2008}}",
+						 @"BibTex format error for RIS record, Pinkoski 2008");
+	STAssertEqualObjects([b2 bibTeXStringWithOptions:BDSKBibTeXOptionDropInternalMask],
+						 @"@article{cite-key,\n\tAuthor = {Marc Pinkoski},\n\tJournal = {Histories of Anthropology Annual},\n\tPages = {172--204},\n\tTitle = {Julian Steward, American Anthropology, and Colonialism},\n\tVolume = {4},\n\tYear = {2008}}",
+						 @"BibTex format error for RIS record, Pinkoski 2008");
+}
+
+@end
