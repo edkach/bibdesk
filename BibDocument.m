f539fb755982cd7a8640a9898f99b03e385b5262
jefferis <jefferis@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2009-01-16 12:33:59 +0000

    Fix reversion of PubMed lookup for dropped <PMID>.pdf files
    - svn 14067 restricted the search to [AID] field but this only contains
      DOIs or published supplied IDs.
    - PMID is contained in [PMID] field.
    
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@14070 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibDocument.m
+++ BibDocument.m
@@ -2415,7 +2415,7 @@ originalContentsURL:(NSURL *)absoluteOriginalContentsURL
             NSString *lastPathComponent = [[fnStr lastPathComponent] stringByDeletingPathExtension];
             BOOL tryPubMed = [[OFPreferenceWrapper sharedPreferenceWrapper] boolForKey:BDSKShouldUsePubMedMetadata];
             if(newBI == nil && tryPubMed && [lastPathComponent containsCharacterInSet:[NSCharacterSet nonDecimalDigitCharacterSet]] == NO)
-                newBI = [BibItem itemWithPubMedSearchTerm:[NSString stringWithFormat:@"%@ [AID]", lastPathComponent]];
+                newBI = [BibItem itemWithPubMedSearchTerm:[NSString stringWithFormat:@"%@ [PMID]", lastPathComponent]];
 
 			// GJ try parsing pdf to extract info that is then used to get a PubMed record
 			if(newBI == nil && [[[NSWorkspace sharedWorkspace] UTIForURL:url] isEqualToUTI:(NSString *)kUTTypePDF] && [[OFPreferenceWrapper sharedPreferenceWrapper] boolForKey:BDSKShouldParsePDFToGeneratePubMedSearchTerm])
