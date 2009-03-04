74bab05ba8be3e1a29fd6ada8500f569c8a31de8
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2009-03-04 23:05:52 +0000

    implement method in the correct class
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@14611 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKSharingClient.h
+++ BDSKSharingClient.h
@@ -49,8 +49,6 @@
     BDSKSharingClientServer *server;
 }
 
-+ (NSString *)keychainServiceNameWithComputerName:(NSString *)computerName;
-
 - (id)initWithService:(NSNetService *)aService;
 
 - (NSData *)archivedPublications;
