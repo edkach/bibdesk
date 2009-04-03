8e10ecd262d558a77f2b7f9bae40772edd640b20
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2009-04-03 15:09:37 +0000

    Disable new web scrapers, because there's an exception
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@14930 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKWebParser.m
+++ BDSKWebParser.m
@@ -68,10 +68,12 @@ static Class webParserClassForType(int stringType)
             return [BDSKSpiresParser class];
 		case BDSKArxivWebType: 
             return [BDSKArxivParser class];
-		case BDSKMathSciNetWebType: 
+		/*
+        case BDSKMathSciNetWebType: 
             return [BDSKMathSciNetParser class];
 		case BDSKZentralblattWebType: 
             return [BDSKZentralblattParser class];
+        */
         default:
             return Nil;
     }    
@@ -92,10 +94,12 @@ static Class webParserClassForType(int stringType)
 		return BDSKSpiresWebType;
     if([BDSKArxivParser canParseDocument:domDocument xmlDocument:xmlDocument fromURL:url])
 		return BDSKArxivWebType;
+    /*
     if([BDSKMathSciNetParser canParseDocument:domDocument xmlDocument:xmlDocument fromURL:url])
 		return BDSKMathSciNetWebType;
     if([BDSKZentralblattParser canParseDocument:domDocument xmlDocument:xmlDocument fromURL:url])
 		return BDSKZentralblattWebType;
+    */
     return BDSKUnknownWebType;
 }
 
