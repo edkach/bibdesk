54ea0122ebd647d8b5d18e665b71a2b99dc17044
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2009-07-29 09:21:14 +0000

    don't log failing to parse a web page, as there are many pages we can't parse and it's driving me crazy
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@15599 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKWebGroupViewController.m
+++ BDSKWebGroupViewController.m
@@ -536,8 +536,8 @@ static inline void addMatchesFromBookmarks(NSMutableArray *bookmarks, BDSKBookma
         }
         if (nil == newPubs) {
             // !!! logs are here to help diagnose problems that users are reporting
-            NSLog(@"-[%@ %@] %@", [self class], NSStringFromSelector(_cmd), error);
-            NSLog(@"loaded MIME type %@", [[dataSource mainResource] MIMEType]);
+            //NSLog(@"-[%@ %@] %@", [self class], NSStringFromSelector(_cmd), error);
+            //NSLog(@"loaded MIME type %@", [[dataSource mainResource] MIMEType]);
             // !!! what to do here? if user clicks on a PDF, we're loading application/pdf, which is clearly not an error from the user perspective...so should the error only be presented for text/plain?
             //[NSApp presentError:error];
         }
