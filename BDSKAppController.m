8f7d4cca0cf040923b546eb66d0e933666ebd8ff
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2009-12-14 12:14:10 +0000

    register help book, because some methods don't automatically register it, e.g. the help link in the web group
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@16332 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKAppController.m
+++ BDSKAppController.m
@@ -291,6 +291,11 @@ static void fixLegacyTableColumnIdentifiers()
     // validate the Cite Key and LocalUrl format strings
     [self checkFormatStrings];
     
+    // register our help book, so it's available for methods that don't register this, e.g. the web group
+    FSRef appRef;
+    if (noErr == FSPathMakeRef((const UInt8 *)[[[NSBundle mainBundle] bundlePath] fileSystemRepresentation], &appRef, NULL))
+        AHRegisterHelpBook(&appRef);
+    
     // register URL handler
     [[NSAppleEventManager sharedAppleEventManager] setEventHandler:self andSelector:@selector(handleGetURLEvent:withReplyEvent:) forEventClass:kInternetEventClass andEventID:kAEGetURL];
     
