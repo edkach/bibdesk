db0b8b3d07a807069f3ed8bdf9739b7267a4b923
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2010-10-02 09:41:05 +0000

    use unique string for temp dir used for template preview, because webkit caches the css when the full path remains the same
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@17169 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BibDocument_UI.m
+++ BibDocument_UI.m
@@ -160,7 +160,7 @@ enum {
         // we generally assume UTF-8 encoding for all template-related files
         if ([accessoryURLs count] > 0) {
             NSFileManager *fm = [NSFileManager defaultManager];
-            NSString *tmpDir = [fm makeTemporaryDirectoryWithBasename:@"templatepreview"];
+            NSString *tmpDir = [fm makeTemporaryDirectoryWithBasename:[[NSProcessInfo processInfo] globallyUniqueString]];
             NSURL *tmpDirURL = [NSURL fileURLWithPath:tmpDir];
             for (NSURL *accessoryURL in accessoryURLs)
                 [fm copyObjectAtURL:accessoryURL toDirectoryAtURL:tmpDirURL error:NULL];
