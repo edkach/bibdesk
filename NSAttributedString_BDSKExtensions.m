0c2fdb53b6db613ac374a50edee13af8146e0f66
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2011-10-07 13:10:38 +0000

    use appkit extension to read RTF data
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@18162 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- NSAttributedString_BDSKExtensions.m
+++ NSAttributedString_BDSKExtensions.m
@@ -266,12 +266,8 @@ static void BDSKApplyAttributesToString(const void *value, void *context)
 }
 
 - (void)setScriptingRTF:(id)data {
-    if (data) {
-        NSAttributedString *attrString = [[NSAttributedString alloc] initWithData:data options:[NSDictionary dictionary] documentAttributes:NULL error:NULL];
-        if (attrString)
-            [self setAttributedString:attrString];
-        [attrString release];
-    }
+    if (data)
+        [self readFromData:data options:[NSDictionary dictionary] documentAttributes:NULL error:NULL];
 }
 
 @end
