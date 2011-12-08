974a675016f3687d47ddb404eeb25755b978b99f
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2011-12-08 22:50:19 +0000

    move dragging source operation to correct class
    git-svn-id: https://bibdesk.svn.sourceforge.net/svnroot/bibdesk/trunk/bibdesk@18302 0fceea05-a30d-0410-8a8b-80ef821fb0a1

--- BDSKAddressTextField.m
+++ BDSKAddressTextField.m
@@ -185,6 +185,11 @@
     [super mouseDown:theEvent];
 }
 
+// flag changes during a drag are not forwarded to the application, so we fix that at the end of the drag
+- (void)draggedImage:(NSImage *)anImage endedAt:(NSPoint)aPoint operation:(NSDragOperation)operation{
+    [[NSNotificationCenter defaultCenter] postNotificationName:BDSKFlagsChangedNotification object:NSApp];
+}
+
 #if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
 - (id <BDSKAddressTextFieldDelegate>)delegate { return (id <BDSKAddressTextFieldDelegate>)[super delegate]; }
 - (void)setDelegate:(id <BDSKAddressTextFieldDelegate>)newDelegate { [super setDelegate:newDelegate]; }
--- BDSKDragTextField.m
+++ BDSKDragTextField.m
@@ -80,11 +80,6 @@
     return [super setKeyboardFocusRingNeedsDisplayInRect:[self bounds]];
 }
 
-// flag changes during a drag are not forwarded to the application, so we fix that at the end of the drag
-- (void)draggedImage:(NSImage *)anImage endedAt:(NSPoint)aPoint operation:(NSDragOperation)operation{
-    [[NSNotificationCenter defaultCenter] postNotificationName:BDSKFlagsChangedNotification object:NSApp];
-}
-
 #pragma mark Delegate
 
 #if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
